3eaf0329b66c38fbb103e11c129de7ac5f8e93f4
dianabanguera <dianabangueravillalobo@gmail.com>
2022-04-19 17:16:06 -0700

    Get function

--- get_shell_function.c
+++ get_shell_function.c
@@ -5,19 +5,19 @@
  * @sh: variable
  * Return: Null
  */
-int (*get_shell_function(char *sh))(void)
+int (*get_shell_function(char *s))(void)
 {
 	int i = 0;
 
 	sbuilt_t bin[] = {
-		{"env", shell_enviroment};
-		{"exit", sh_exit};
-		{NULL, NULL};
-	}
+		{"env", shell_enviroment},
+		{"exit", shel_exit},
+		{NULL, NULL}
+	};
 
 	while (bin[i].var != NULL)
 	{
-		if ((strcmmp_(sh, bin[i].var)) == 0)
+		if ((strcmmp_(s, bin[i].var)) == 0)
 		{
 			return (bin[i].f);
 		}
