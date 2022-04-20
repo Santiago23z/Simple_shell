070594c7b5ae6296aeb802e30e72606a9a20cf6d
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-04-20 10:13:40 -0500

    Path

---
+++ path.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * path_ruta - path
+ * @pth: arguments
+ * Return: *
+ */
+char *path_ruta(char *pth)
+{
+    char *rut = NULL;
+    int x = 0;
+
+    rut = malloc(sizeof(char) * BUFF_sz);
+    if (rut == NULL)
+    {
+        perror("failing malloc")
+        exit(EXIT_FAILURE);
+    }
+    while (pth[x] != '\0')
+    {
+        if (pth[x] == '/')
+        {
+            rut = path;
+            break;
+        }
+        x++;
+    }
+    free(rut);
+    return (rut);
+}
