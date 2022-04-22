08b1e940d1f84343e21f05cfdb75163cd562c911
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-04-21 20:54:37 -0500

    shell

--- prompt.c
+++ prompt.c
@@ -11,8 +11,10 @@ char *prompt(void)
 	size_t size = 0;
 	int index = 0;
 
-
-	write(1, "My_prompt$ ", 11);
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		write(1, "My_prompt$ ", 11);
+	}
 	index = getline(&l, &size, stdin);
 
 	if (index == -1)
